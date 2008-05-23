454e52272d040d0bdbf94a6d9511f77fd3dc86de
kiorky <devnull@localhost>
2008-05-23 08:34:03 +0200

    finnished for 0.4

--- hooks/libxml.py
+++ hooks/libxml.py
@@ -1,19 +1,57 @@
 import os
 import re
-myos=os.uname()[0]
+import shutil
 
-def libxml2(options,buildout):
-    # patch Makefile
+def install(options,buildout):
+    """Installs only the python site-packages."""
+    pwd = options['location']
+    os.system("""
+              cd %s
+              cd python
+              make install""" % pwd)
+
+    for d in 'share', 'bin':
+        if os.path.exists(
+            os.path.join(pwd, d)):
+            shutil.rmtree(
+                os.path.join(pwd, d))
+
+
+def libxml(options, buildout, version):
+    """Patch Makefile to point to our site packages."""
     pwd=options['compile-directory']
-    file=open('/'.join((pwd,'python','Makefile')))
-    import pdb;pdb.set_trace()  ## Breakpoint ##
+    MAKEFILE = os.path.join(
+        pwd,
+        'python',
+        'Makefile'
+    )
+    file=open(MAKEFILE)
     lines=file.readlines()
     for i,line in enumerate(lines):
-        if line[:9] == 'LDFLAGS =' or line[:20] == 'PYTHON_SITE_PACKAGES':
-            lines[i]=re.sub('2.4','2.5',line)
-    file=open(os.path.join(pwd, 'python', 'Makefile'),'w+')
+        for word in 'pythondir', 'PYTHON_SITE_PACKAGES':
+            if line.startswith(word):
+                lines[i] = re.sub(
+                    '%s.*' % word,
+                    '%s = %s' % (
+                        word,
+                        os.path.join(
+                            buildout['buildout']['directory'],
+                            'parts',
+                            'site-packages-%s' % version)
+                    ),
+                    line
+                )
+    file = open(MAKEFILE,'w+')
     file.writelines(lines)
     file.close()
-    return None 
+    os.chdir(pwd)
+
+def libxml2_24(options,buildout):
+    """Patch Makefile to point to our site packages."""
+    libxml(options, buildout, '2.4')
+
+def libxml2_25(options,buildout):
+    """Patch Makefile to point to our site packages."""
+    libxml(options, buildout, '2.5')
 
 # vim:set ts=4 sts=4 et  :
